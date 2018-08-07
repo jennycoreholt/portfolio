4e6ef0aadd61e0c1c457f74bd95de7529af99665
Jenny Core-Holt <jenny@jennycore.com>
2018-08-07 14:35:16 +0200

    file clean up  and animations added

--- js/main.js
+++ js/main.js
@@ -1,30 +1,7 @@
-$(document).ready(function () {
-  $('.dropdown-toggle').dropdown();
+$(document).ready(function(){
 
-  $('#nav-wrapper').height($("#mainnav").height());
+/*! Fades in whole page on load */
+$('body').css('display', 'none');
+$('body').fadeIn(600);
 
-  $('#mainnav').affix({
-    offset: { top: $('#mainnav').offset().top }
-  });
-
-  /* --------homepage-btn ----------- */
-    $(".fa-chevron-circle-down").click(function() {
-      $('#mainnav').ScrollTo({
-        duration: 1000,
-        easing: 'easeInOutExpo'
-      });
-    });
-
-    /* -------- THIS IS NEW!!! Nav Bar Scroll THIS IS NEW!!! ---------*/
-
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        var $scrollTo = $($anchor.attr('href'));
-        var $navHeight = 85;
-
-        $('html, body').stop().animate({
-            scrollTop: ($scrollTo.offset().top - $navHeight)
-        }, 1000, 'easeInOutExpo');
-        event.preventDefault();
-    });
 });
